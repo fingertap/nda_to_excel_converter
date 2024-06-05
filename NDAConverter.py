246c26777d695ed32e778aaecacbc39772e7e64a
Han <zh950713@gmail.com>
2024-06-05 11:06:43 +0800

    many fix

--- NDAConverter.py
+++ NDAConverter.py
@@ -17,14 +17,24 @@ def on_progress_complete():
 def run_script():
     reset_progress()
 
+    runtime_path = runtime_entry.get()
     input_folder = input_folder_entry.get()
     output_folder = output_folder_entry.get()
     output_format = output_format_var.get()
+
+    if not runtime_path:
+        tk.messagebox.showerror("Error", "Please select the location of BTSDAExReport.exe!")
+        return
+    runtime = Path(runtime_path)
+    if not runtime.exists():
+        tk.messagebox.showerror("Error", "BTSDAExReport.exe not found.")
+        return
 
     if not input_folder:
         tk.messagebox.showerror("Error", "Please select an input folder.")
         return
     input_folder = Path(input_folder)
+
     if not output_folder:
         tk.messagebox.showerror("Error", "Please select an output folder.")
         return
@@ -32,14 +42,9 @@ def run_script():
     if not output_folder.exists():
         output_folder.mkdir(parents=True)
 
-    runtime = Path('./BTSDAExReport.exe')
-    if not runtime.exists():
-        tk.messagebox.showerror("Error", "BTSDAExReport.exe not found.")
-        return
 
     progress_bar["maximum"] = len(list(input_folder.glob("*.nda*")))
 
-    # Simulate a long-running task
     for index, file in enumerate(input_folder.glob("*.nda*")):
         target_file = output_folder / f"{file.stem}.xlsx"
         cmd = f'"{runtime}" export {output_format} "{file}" "{target_file}"'
@@ -58,40 +63,54 @@ def run_script():
 root = tk.Tk()
 root.title("Converter GUI for Neware NDA files")
 
-tk.Label(root, text="Input Folder:").grid(row=0, column=0)
+# First row: select the BTSDAExReport.exe file
+tk.Label(root, text="BTSDAExReport.exe path:").grid(row=0, column=0)
+runtime_entry = tk.Entry(root)
+runtime_entry.grid(row=0, column=1)
+tk.Button(
+    root,
+    text="Browse",
+    command=lambda: runtime_entry.delete(0, tk.END)
+    or runtime_entry.insert(0, filedialog.askdirectory()),
+).grid(row=0, column=2)
+
+# Second row: input folder
+tk.Label(root, text="Input Folder:").grid(row=1, column=0)
 input_folder_entry = tk.Entry(root)
-input_folder_entry.grid(row=0, column=1)
+input_folder_entry.grid(row=1, column=1)
 tk.Button(
     root,
     text="Browse",
     command=lambda: input_folder_entry.delete(0, tk.END)
     or input_folder_entry.insert(0, filedialog.askdirectory()),
-).grid(row=0, column=2)
+).grid(row=1, column=2)
 
-tk.Label(root, text="Output Folder:").grid(row=1, column=0)
+# Third row: output folder
+tk.Label(root, text="Output Folder:").grid(row=2, column=0)
 output_folder_entry = tk.Entry(root)
-output_folder_entry.grid(row=1, column=1)
+output_folder_entry.grid(row=2, column=1)
 tk.Button(
     root,
     text="Browse",
     command=lambda: output_folder_entry.delete(0, tk.END)
     or output_folder_entry.insert(0, filedialog.askdirectory()),
-).grid(row=1, column=2)
+).grid(row=2, column=2)
 
-tk.Label(root, text="Output Format:").grid(row=2, column=0)
+# Fourth row: output format dropdown list
+tk.Label(root, text="Output Format:").grid(row=3, column=0)
 output_format_var = tk.StringVar()
 tk.OptionMenu(root, output_format_var, "General", "Layer", "Custom", "Sim").grid(
-    row=2, column=1
+    row=3, column=1
 )
 output_format_var.set("Custom")
+tk.Button(root, text="Convert!", command=run_script).grid(row=3, column=2)
 
+# Last row: progress bar
 style = ttk.Style()
-
-tk.Label(root, text="Progress:").grid(row=3, column=0)
+tk.Label(root, text="Progress:").grid(row=4, column=0)
 progress_bar = ttk.Progressbar(
     root, orient="horizontal", length=200, mode="determinate"
 )
-progress_bar.grid(row=3, columnspan=2, column=1)
-tk.Button(root, text="Convert!", command=run_script).grid(row=2, column=2)
+progress_bar.grid(row=4, columnspan=2, column=1)
 
 root.mainloop()
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Neware NDA File Converter
 
 This tool provieds batch converting functionality from nda files to excel files with a simple GUI. Please download the application from the [release](https://github.com/fingertap/nda_to_excel_converter/releases) page.
+
+You should have installed a `BTSDAExReport.exe` somewhere in your computer to use this converter. We do not provide the original software. Please respect their copyrights.
